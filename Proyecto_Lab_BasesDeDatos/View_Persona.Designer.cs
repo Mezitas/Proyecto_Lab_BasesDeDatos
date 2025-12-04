f934f8c0f265a6a051f30ae045530c117cb99282
Juan Meza <diegopaz04717@gmail.com>
2025-12-04 12:48:03 -0500

    Cambios Primarios - Necesarios
    Cambio el Form1 a FormMain.
    Creacion de los userControls necesarios para las pestañas, aun no se agregan como manejar los uCs, pero poco a poco.

---
+++ Proyecto_Lab_BasesDeDatos/View_Persona.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Proyecto_Lab_BasesDeDatos
+{
+    partial class View_Persona
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            SuspendLayout();
+            // 
+            // View_Persona
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "View_Persona";
+            Size = new Size(758, 465);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
